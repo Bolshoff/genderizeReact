d03ea53188e444b561c74cf97017b7869a706213
Aleksei <abolshoff@yandex.ru>
2022-05-26 15:00:37 +0900

    added events focus & change input

--- src/TextInput.js
+++ src/TextInput.js
@@ -3,10 +3,23 @@ import './TextInput.css';
 class TextInput extends React.Component{
   constructor(props) {
     super(props);
-    this.state = {value:""}
+    this.state = {value:""};
+    this.inputChangeListener = this.inputChangeListener.bind(this);
+    this.isFocusedInput = this.isFocusedInput.bind(this);
+  }
+  inputChangeListener(){
+    console.log('Xnj-nj ghjbc[jlbn');
+  }
+  isFocusedInput(){
+    console.log('focus pocus');
   }
   render() {
-    return <input className="name-input" type="text" placeholder="type name"/>
+    return( <input className="name-input"
+                   type="text"
+                   placeholder="type name"
+                   onChange={this.inputChangeListener}
+                   onFocus={this.isFocusedInput}
+            />);
   }
 }
 
