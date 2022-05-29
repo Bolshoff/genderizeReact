c926485d1fdccd3ac59214a31ab8d928efbebd6e
Aleksei <abolshoff@yandex.ru>
2022-05-30 08:13:04 +0900

    before pull

--- src/Main.js
+++ src/Main.js
@@ -1,27 +1,53 @@
+import React from 'react';
 import "./Main.css"
 import TextInput from './TextInput';
 import Button from './Button';
 import GenderResponse from './GenderResponse';
 
-function Main(props){
+class Main extends React.Component {
+  constructor(props) {
+    super(props);
+    this.checkGender = this.checkGender.bind(this);
 
-  function checkGender(e){
-    e.preventDefault();
-    console.log('gender is checked');
-    e.target.reset();
 
   }
-  return(
-      <div className="genderize">
-        <GenderResponse />
-        <form className="submit-name__form" onSubmit={checkGender}>
-          <label>
-            <TextInput value="" />
-          </label>
-          <Button type="submit" value="CHECK"/>
-        </form>
-      </div>
-  )
+  // const serverUrl = 'https://api.genderize.io';
+  // let response;
+
+  // async function getGender(){
+  //   const firstName = domElements.inputName.value;
+  //   const url = `${serverUrl}?name=${firstName}`;
+  //
+  //
+  //   response = fetch(url)
+  //   .then(response => name = response.json()  )
+  //   .then(name=>{
+  //
+  //     domElements.outputGender.value = `${firstName} - ${name.gender}`;
+  //   })
+  // }
+
+
+   checkGender(e){
+    e.preventDefault();
+    console.log('gender is checked');
+
+
+  }
+  render() {
+
+    return (
+        <div className="genderize">
+          <GenderResponse/>
+          <form className="submit-name__form" onSubmit={this.checkGender}>
+            <label>
+              <TextInput />
+            </label>
+            <Button type="submit" value="CHECK"/>
+          </form>
+        </div>
+    );
+  }
 }
 
 export default Main;
--- src/TextInput.js
+++ src/TextInput.js
@@ -5,20 +5,21 @@ class TextInput extends React.Component{
     super(props);
     this.state = {value:""};
     this.inputChangeListener = this.inputChangeListener.bind(this);
-    this.isFocusedInput = this.isFocusedInput.bind(this);
+
   }
-  inputChangeListener(){
-    console.log('Xnj-nj ghjbc[jlbn');
+  inputChangeListener(e){
+    this.setState({value: e.target.value});
+    console.log(this.state.value);
   }
-  isFocusedInput(){
-    console.log('focus pocus');
-  }
+
   render() {
+    const inputName = this.props.value;
     return( <input className="name-input"
+                   value={this.state.value}
                    type="text"
                    placeholder="type name"
                    onChange={this.inputChangeListener}
-                   onFocus={this.isFocusedInput}
+
             />);
   }
 }
